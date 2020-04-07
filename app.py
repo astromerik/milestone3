69b746f0b97889f5f3bc9c57c493021325b83258
Erik Åström <astromerik93@gmail.com>
2020-04-07 15:40:19 +0000

    installation of pymongo, flask and dnspython. Connected to database

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 import os
-from flask import Flask, render_template
-import pymongo
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "components"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html", projects=mongo.db.caseBrand.find_one())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+
+
+
+#flask, pymongo, dnspython and flask-pymongo are installed
