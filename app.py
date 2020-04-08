0c52ab8dcfdd4e286af90df6feb404db74a1ea9c
Erik Åström <astromerik93@gmail.com>
2020-04-08 09:16:02 +0000

    added filter forms who loops through the database in order to filter projects on project.html

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ def home():
     return render_template("home.html", projects=mongo.db.projects.find_one())
 
 
+@app.route('/projects')
+def projects():
+    return render_template("projects.html",
+                           casematerial=mongo.db.caseMaterial.find(),
+                           size=mongo.db.layoutSize.find(),
+                           layout=mongo.db.layout.find())
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
