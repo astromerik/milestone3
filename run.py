470ec33520502a3eae783cdfe2735985de121cd8
Erik Åström <astromerik93@gmail.com>
2020-04-18 21:58:05 +0000

    turned off debug

--- run.py
+++ run.py
@@ -223,5 +223,4 @@ def profile():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+            port=int(os.environ.get('PORT')))
